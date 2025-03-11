b1570f574d323d27debbd6400799983b8b20d124
Rahul Prabhu <rahulprabhu198@gmail.com>
2025-03-11 19:17:10 -0400

    Added dependencies and boiler-plate code

---
+++ Roomly/src/main/java/com/roomly/Roomly/RoomlyApplication.java
@@ -0,0 +1,12 @@
+package com.roomly.Roomly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomlyApplication.class, args);
+	}
+}
---
+++ Roomly/src/test/java/com/roomly/Roomly/RoomlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomly.Roomly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
