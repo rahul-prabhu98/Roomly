eb07704eaa863f7cdfed87c8f578f82567746cee
Rahul Prabhu <rahulprabhu198@gmail.com>
2025-03-11 20:47:41 -0400

    Created the room entity

--- Roomly/src/main/java/com/roomly/Roomly/entity/Booking.java
+++ Roomly/src/main/java/com/roomly/Roomly/entity/Booking.java
@@ -2,29 +2,71 @@ package com.roomly.Roomly.entity;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 @Entity
-@Table(name = "booking")
+@Table(name = "bookings")
 public class Booking {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Check In date is required")
     private LocalDate checkInDate;
+
+    @Future(message = "Check out date must be in the future")
     private LocalDate checkOutDate;
 
+    @Min(value = 1, message = "Number of adults must not be less than 1")
     private int numOfAdults;
+
+    @Min(value = 0, message = "Number of children must not be less than 0")
     private int numOfChildren;
+
     private int totalNumOfGuests;
+
     private String bookingConfirmationCode;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
     private User user;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
     private Room room;
 
+    public void calculateTotalNumOfGuest(){
+        this.totalNumOfGuests = this.numOfAdults + this.numOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumOfGuest();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumOfGuest();
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "id=" + id +
+                ", checkInDate=" + checkInDate +
+                ", checkOutDate=" + checkOutDate +
+                ", numOfAdults=" + numOfAdults +
+                ", numOfChildren=" + numOfChildren +
+                ", totalNumOfGuests=" + totalNumOfGuests +
+                ", bookingConfirmationCode='" + bookingConfirmationCode + '\'' +
+                '}';
+    }
 }
---
+++ Roomly/src/main/java/com/roomly/Roomly/entity/Room.java
@@ -0,0 +1,37 @@
+package com.roomly.Roomly.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String bookingConfirmationCode;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private String roomPhotoUrl;
+    private String roomDescription;
+    private List<Booking> bookings = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", bookingConfirmationCode='" + bookingConfirmationCode + '\'' +
+                ", roomType='" + roomType + '\'' +
+                ", roomPrice=" + roomPrice +
+                ", roomPhotoUrl='" + roomPhotoUrl + '\'' +
+                ", roomDescription='" + roomDescription + '\'' +
+                '}';
+    }
+}
