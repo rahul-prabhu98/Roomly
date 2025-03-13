e200c76d96427cf98d485671283159fb6d691b19
Rahul Prabhu <rahulprabhu198@gmail.com>
2025-03-12 23:33:54 -0400

    Created the user data transfer object

---
+++ Roomly/src/main/java/com/roomly/Roomly/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package com.roomly.Roomly.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.roomly.Roomly.entity.Booking;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO {
+
+    private Long id;
+    private String email;
+    private String name;
+    private String phoneNumber;
+    private String role;
+    private List<Booking> bookings = new ArrayList<>();
+
+}
