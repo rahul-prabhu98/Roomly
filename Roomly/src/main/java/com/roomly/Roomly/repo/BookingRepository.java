40920aa68a1c9091644eb8e4e4dc0cd0e390f9e8
Rahul Prabhu <rahulprabhu198@gmail.com>
2025-03-13 00:31:15 -0400

    Created the Booking repository

---
+++ Roomly/src/main/java/com/roomly/Roomly/repo/BookingRepository.java
@@ -0,0 +1,16 @@
+package com.roomly.Roomly.repo;
+
+import com.roomly.Roomly.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findByRoomId(Long roomId);
+
+    List<Booking> findByBookingConfirmationCode(String confirmationCode);
+
+    List<Booking> findByUserId(Long id);
+
+}
