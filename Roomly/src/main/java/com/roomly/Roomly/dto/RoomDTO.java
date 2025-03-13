e6262bc7c37e5bdeeb6d9bc7f01c80c704a7076f
Rahul Prabhu <rahulprabhu198@gmail.com>
2025-03-13 00:17:30 -0400

    Created the user repository

--- Roomly/src/main/java/com/roomly/Roomly/dto/RoomDTO.java
+++ Roomly/src/main/java/com/roomly/Roomly/dto/RoomDTO.java
@@ -12,6 +12,7 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class RoomDTO {
 
+    private Long id;
     private String roomType;
     private BigDecimal roomPrice;
     private String roomPhotoUrl;
