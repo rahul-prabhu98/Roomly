8ef8057b537afb817dcdc4b240f1d86739454eee
Rahul Prabhu <rahulprabhu198@gmail.com>
2025-03-17 12:16:23 -0400

    Created the user service interface

---
+++ Roomly/src/main/java/com/roomly/Roomly/service/interfac/IUserService.java
@@ -0,0 +1,21 @@
+package com.roomly.Roomly.service.interfac;
+
+import com.roomly.Roomly.dto.LoginRequest;
+import com.roomly.Roomly.dto.Response;
+import com.roomly.Roomly.entity.User;
+
+public interface IUserService {
+    Response register(User user);
+
+    Response login(LoginRequest loginRequest);
+
+    Response getAllUsers();
+
+    Response getUserBookingHistory(String userId);
+
+    Response deleteUser(String userId);
+
+    Response getUserById(String userId);
+
+    Response getMyInfo(String email);
+}
