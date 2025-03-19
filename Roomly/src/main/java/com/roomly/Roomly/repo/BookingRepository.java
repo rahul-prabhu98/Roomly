7c6f7617c06f74e8f428fed3b4377d6476925420
Rahul Prabhu <rahulprabhu198@gmail.com>
2025-03-19 00:29:54 -0400

    Created the implementation for booking service

--- Roomly/src/main/java/com/roomly/Roomly/repo/BookingRepository.java
+++ Roomly/src/main/java/com/roomly/Roomly/repo/BookingRepository.java
@@ -4,13 +4,16 @@ import com.roomly.Roomly.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     List<Booking> findByRoomId(Long roomId);
 
-    List<Booking> findByBookingConfirmationCode(String confirmationCode);
+    //List<Booking> findByBookingConfirmationCode(String confirmationCode);
 
     List<Booking> findByUserId(Long id);
+
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
 
 }
